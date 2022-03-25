174994e694f96b45496d401110fe7e25da39914f
Max Klenk <max@klenk.biz>
2022-03-25 18:12:40 +0100

    add staking logic

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import Lifi, { ChainId, Execution, ExecutionSettings, RoutesRequest } from '@lifinance/sdk'
-import { ethers } from 'ethers'
+import { BigNumberish, ethers } from 'ethers'
 import 'dotenv/config'
+import axios from 'axios'
+
+const STAKE_KLIMA_CONTRACT_ADDRESS = '0x4D70a031Fc76DA6a9bC0C922101A05FA95c3A227'
+const KLIMA_ADDRESS = '0x4e78011Ce80ee02d2c3e649Fb657E45898257815'
+const sKLIMA_ADDRESS = '0xb0C22d8D350C67420f06F48936654f567C73E8C8'
 
 const setupWallet = (chainId: number, providerUri: string) => {
   // setup wallet
@@ -22,6 +27,76 @@ const setupWallet = (chainId: number, providerUri: string) => {
   return wallet
 }
 
+const getGasPrice = async (chainId: number) => {
+  const map: Record<number, string> = {
+    [ChainId.POL]: 'poly',
+    [ChainId.FTM]: 'ftm',
+  }
+  const response = await axios.get(`https://owlracle.info/${map[chainId]}/gas`)
+  return ethers.utils.parseUnits(response.data.speeds[2].gasPrice.toString(), 9)
+}
+
+const getSetAllowanceTransaction = async (tokenAddress: string, approvalAddress: string, amount: BigNumberish) => {
+  const ERC20_ABI = [
+    {
+      "name": "approve",
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "spender",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        }
+      ],
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "name": "allowance",
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "owner",
+          "type": "address"
+        },
+        {
+          "internalType": "address",
+          "name": "spender",
+          "type": "address"
+        }
+      ],
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    }
+  ]
+
+  const erc20 = new ethers.Contract(tokenAddress, ERC20_ABI)
+  return erc20.populateTransaction.approve(approvalAddress, amount)
+}
+
+const getStakeKlimaTransaction = (amount: BigNumberish) => {
+  const stakeKlimaABI = [{ "inputs": [{ "internalType": "address", "name": "_staking", "type": "address" }, { "internalType": "address", "name": "_KLIMA", "type": "address" }], "stateMutability": "nonpayable", "type": "constructor" }, { "inputs": [], "name": "KLIMA", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "_amount", "type": "uint256" }], "name": "stake", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "staking", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }]
+  const contract = new ethers.Contract(STAKE_KLIMA_CONTRACT_ADDRESS, stakeKlimaABI)
+  return contract.populateTransaction.stake(amount)
+}
 
 const run = async () => {
   const chains = await Lifi.getChains()
@@ -30,7 +105,8 @@ const run = async () => {
   const tokenFantomUSDC = await Lifi.getToken(fantom.id, 'USDC')!
   const tokenPolygonUSDC = await Lifi.getToken(polygon.id, 'USDC')!
   const tokenPolygonMATIC = await Lifi.getToken(polygon.id, 'MATIC')!
-  const tokenPolygonKLIMA = await Lifi.getToken(polygon.id, '0x4e78011Ce80ee02d2c3e649Fb657E45898257815')!
+  const tokenPolygonKLIMA = await Lifi.getToken(polygon.id, KLIMA_ADDRESS)!
+  const tokenPolygonSKLIMA = await Lifi.getToken(polygon.id, sKLIMA_ADDRESS)!
 
   // Setup Wallet
   const wallet = setupWallet(fantom.id, fantom.metamask.rpcUrls[0])
@@ -123,9 +199,42 @@ const run = async () => {
   const tokenAmountKlima = (await Lifi.getTokenBalance(smartWalletPolygonAddress, tokenPolygonKLIMA))!
   const amountKlima = ethers.utils.parseUnits(tokenAmountKlima.amount, tokenAmountKlima.decimals)
   console.log(`Wallet contains ${amountKlima.toString()} KLIMA.`)
+  const walletPolygon = setupWallet(polygon.id, polygon.metamask.rpcUrls[0])
+
+  // DEBUG:
+  const gasPrice = await getGasPrice(ChainId.POL)
+  console.log(`Current gas price is ${gasPrice.toString()}`)
 
   // Transaction 2:
-  // > TODO: approve & stake Klima tokens and let the LP tokens be sent to the key based wallet
+  // > TODO: approve KLIMA tokens
+  // approve KLIMA: e.g. https://polygonscan.com/tx/0xb1aca780869956f7a79d9915ff58fd47acbaf9b34f0eb13f9b18d1772f1abef2
+  console.log('Approve KLIMA')
+  const txAllow = await getSetAllowanceTransaction(tokenPolygonKLIMA.address, STAKE_KLIMA_CONTRACT_ADDRESS, amountKlima)
+
+  // DEBUG:
+  txAllow.gasPrice = gasPrice
+  const txAllowResponse = await walletPolygon.sendTransaction(txAllow)
+  console.log(txAllowResponse)
+  const txAllowReceipt = await txAllowResponse.wait()
+  console.log(txAllowReceipt)
+
+  // > TODO: stake Klima tokens (sender received sKLIMA tokens 1:1)
+  // stake KLIMA: e.g. https://polygonscan.com/tx/0x5c392aa3487a1fa9e617c5697fe050d9d85930a44508ce74c90caf1bd36264bf
+  console.log('Stake KLIMA')
+  const txStake = await getStakeKlimaTransaction(amountKlima)
+
+  // DEBUG:
+  txStake.gasPrice = gasPrice
+  const txStakeResponse = await walletPolygon.sendTransaction(txStake)
+  console.log(txStakeResponse)
+  const txStakeReceipt = await txStakeResponse.wait()
+  console.log(txStakeReceipt)
+
+  // > TODO: send sKLIMA token to Key Based Wallet
+  console.log('Send sKLIMA')
+  const tokenAmountSKlima = (await Lifi.getTokenBalance(smartWalletPolygonAddress, tokenPolygonSKLIMA))!
+  const amountSKlima = ethers.utils.parseUnits(tokenAmountSKlima.amount, tokenAmountSKlima.decimals)
+  console.log(`Wallet contains ${amountSKlima.toString()} sKLIMA.`)
 }
 
 run()
